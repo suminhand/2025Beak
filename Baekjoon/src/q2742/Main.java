7bc66d1a97d6e748ea128ae029ca7128bdaddd2f
SYU <SYU@DESKTOP-1VHBQ5H>
2024-12-30 14:17:47 +0900

    Initial Commit

---
+++ Baekjoon/src/q2742/Main.java
@@ -0,0 +1,18 @@
+package q2742;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int N;
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		
+		for (int i=N; i>=1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
